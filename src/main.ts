3a747023e360c20fa70d649009f6c8bf7c3c792c
Beautero-ks <beauterokenne37@gmail.com>
2025-12-17 14:24:20 +0100

    add the cors prod mode

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://petite-annonce.vercel.app/', 'http://localhost:4200'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: ['https://petite-annonce.vercel.app/', 'http://localhost:4200'],
+  //   credentials: true,
+  // });
+
+  app.enableCors({ origin: true });
 
   await app.listen(3000);
 }
