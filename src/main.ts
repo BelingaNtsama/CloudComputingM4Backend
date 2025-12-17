007321351aefb234d6ad3502ca70c660de41b08e
Beautero-ks <beauterokenne37@gmail.com>
2025-12-17 14:46:31 +0100

    fix: remove slash in the cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://petite-annonce.vercel.app/', 'http://localhost:4200'],
+    origin: ['https://petite-annonce.vercel.app', 'http://localhost:4200'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
